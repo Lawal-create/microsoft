70d22dbb5e85639ca5b1397da433a123c904c5aa
Lawal Hakeem <lawizyhal@gmail.com>
2021-12-11 15:06:35 +0100

    fix: PORT ISSUES

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const sendMail = require('./mail')
 
 app.use(express.urlencoded({
